7231b38cbabbf0c9fdba0d87fb8953494a48030e
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2021-01-31 23:52:22 +0330

    Implement gt, lt, eq and ne methods

--- edge.py
+++ edge.py
@@ -3,4 +3,34 @@ class Edge:
     def __init__(self, head, tail):
         self.head = head
         self.tail = tail
+        self.value = 0
 
+    def __eq__(self, other):
+        if other == self:
+            return True
+        if not isinstance(other, Edge):
+            return False
+        return other.value == self.value
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __gt__(self, other):
+        if not isinstance(other, Edge):
+            raise Exception(str(other) + ' is not and instance of ' + self.__class__.__name__)
+        return other.value < self.value
+
+    def __lt__(self, other):
+        if not isinstance(other, Edge):
+            raise Exception(str(other) + ' is not and instance of ' + self.__class__.__name__)
+        return other.value > self.value
+
+    def __ge__(self, other):
+        if not isinstance(other, Edge):
+            raise Exception(str(other) + ' is not and instance of ' + self.__class__.__name__)
+        return other.value <= self.value
+
+    def __le__(self, other):
+        if not isinstance(other, Edge):
+            raise Exception(str(other) + ' is not and instance of ' + self.__class__.__name__)
+        return other.value >= self.value
--- vertex.py
+++ vertex.py
@@ -5,6 +5,37 @@ class Vertex:
         self.x = x
         self.y = y
         self.adjacent_vertices = []
+        self.value = 0
+
+    def __eq__(self, other):
+        if other == self:
+            return True
+        if not isinstance(other, Vertex):
+            return False
+        return other.value == self.value
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __gt__(self, other):
+        if not isinstance(other, Vertex):
+            raise Exception(str(other) + ' is not and instance of ' + self.__class__.__name__)
+        return other.value < self.value
+
+    def __lt__(self, other):
+        if not isinstance(other, Vertex):
+            raise Exception(str(other) + ' is not and instance of ' + self.__class__.__name__)
+        return other.value > self.value
+
+    def __ge__(self, other):
+        if not isinstance(other, Vertex):
+            raise Exception(str(other) + ' is not and instance of ' + self.__class__.__name__)
+        return other.value <= self.value
+
+    def __le__(self, other):
+        if not isinstance(other, Vertex):
+            raise Exception(str(other) + ' is not and instance of ' + self.__class__.__name__)
+        return other.value >= self.value
 
     def __str__(self):
         return '[' + str(self.identity) + ', ' + str(self.x) + ', ' + str(self.y) + ', ' + str(self.adjacent_vertices) + ']'
